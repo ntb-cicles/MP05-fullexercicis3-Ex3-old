6d840b018d230d8b03fa3ba69dc70579e2fe9726
Nil Torrent <nil@DESKTOP-C6FGL56>
2022-11-15 16:42:17 +0100

    Afegit codi de la classe Main (el que es pot executar). Es crea un tauler de joc amb una bola que es mou de dreta a esquerra.

--- JavaAppM5Fe3/src/javaappm5fe3/JavaAppM5Fe3.java
+++ JavaAppM5Fe3/src/javaappm5fe3/JavaAppM5Fe3.java
@@ -14,7 +14,17 @@ public class JavaAppM5Fe3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TaulerJoc a = new TaulerJoc(300,200);
+        Ball b = new Ball(50,50, 10, "GREEN");
+
+        a.addBall(b);
+        b.setXSpeed(3);
+
+        while(true)
+        {
+            a.pause();
+            b.bounce(300,200);
+        }
     }
     
 }
