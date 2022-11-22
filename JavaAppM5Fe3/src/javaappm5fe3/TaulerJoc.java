93cb8e88a12ef74971a0e468a29c2fcc6b455979
ntb-cicles <103062219+ntb-cicles@users.noreply.github.com>
2022-11-22 20:10:34 +0100

    Update TaulerJoc.java
    Retorn al 6è commit

--- JavaAppM5Fe3/src/javaappm5fe3/TaulerJoc.java
+++ JavaAppM5Fe3/src/javaappm5fe3/TaulerJoc.java
@@ -4,7 +4,11 @@
  */
 package javaappm5fe3;
 
+<<<<<<< HEAD
 
+=======
+//import java.awt.event.KeyEvent;
+>>>>>>> parent of bcba9be (Neteja de codi i canvi en dependència errònea en el KeEvent)
 import java.awt.event.WindowEvent; // No utilitza JavaFX i ho hauria de fer?
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,8 +21,13 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+<<<<<<< HEAD
 import java.awt.event.KeyEvent;
 import javafx.scene.input.KeyEvent; // Fer que importi de Java Beans i no de JavaFX
+=======
+import javafx.scene.input.KeyEvent;
+//import javafx.scene.input.KeyEvent; // Fer que importi de Java Beans i no de JavaFX
+>>>>>>> parent of bcba9be (Neteja de codi i canvi en dependència errònea en el KeEvent)
 import javafx.scene.paint.Color;
 //import javafx.stage.WindowEvent;
 import javax.swing.JFrame;
@@ -38,8 +47,8 @@ public class TaulerJoc {
     private final static int MAXIMUM_OBJECTS = 100000;
 
     // Collections of primitives. These now relate 1:1 to JavaFX Nodes, since moving from AWT.
-    private List<Object> addList = new ArrayList<>();
-    private List<Object> removeList = new ArrayList<>();
+    private List<Object> addList = new ArrayList<Object>();
+    private List<Object> removeList = new ArrayList<Object>();
     private Map<Ball, javafx.scene.shape.Circle> balls = new HashMap<>();
     //private Map<Rectangle, javafx.scene.shape.Rectangle> rectangles = new HashMap<>();
     private int objectCount;
@@ -95,15 +104,17 @@ public class TaulerJoc {
     
     private void initFX() {
 
-        EventHandler<KeyEvent> keyDownHandler = (final KeyEvent keyEvent) -> {
-            if (keyEvent.getCode() == KeyCode.UP)
-                up = true;
-            if (keyEvent.getCode() == KeyCode.DOWN)
-                down = true;
-            if (keyEvent.getCode() == KeyCode.LEFT)
-                left = true;
-            if (keyEvent.getCode() == KeyCode.RIGHT)
-                right = true;
+        EventHandler<KeyEvent> keyDownHandler = new EventHandler<KeyEvent>() {
+            public void handle(final KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.UP) 
+                    up = true;
+                if (keyEvent.getCode() == KeyCode.DOWN) 
+                    down = true;
+                if (keyEvent.getCode() == KeyCode.LEFT) 
+                    left = true;
+                if (keyEvent.getCode() == KeyCode.RIGHT) 
+                    right = true;
+            }
         };
 
         EventHandler<KeyEvent> keyUpHandler = new EventHandler<KeyEvent>() {
@@ -141,21 +152,58 @@ public class TaulerJoc {
             {
                 for (Object o: removeList)
                 {
-                    if (o instanceof Ball b)
+                    if (o instanceof Ball)
                     {
+                        Ball b = (Ball) o;
                         javafx.scene.shape.Circle c = balls.get(b);
                         root.getChildren().remove(c);
 
                         balls.remove(b);
                     }
+<<<<<<< HEAD
                                     }
+=======
+
+                    /*
+                    if (o instanceof Rectangle)
+                    {
+                        Rectangle r = (Rectangle) o;
+                        javafx.scene.shape.Rectangle rectangle = rectangles.get(r);
+                        root.getChildren().remove(rectangle);
+
+                        rectangles.remove(r);
+                    }
+                    */
+                }
+>>>>>>> parent of bcba9be (Neteja de codi i canvi en dependència errònea en el KeEvent)
 
                 removeList.clear();
 
                 // Add any new objects to the scene.
                 for (Object o: addList)
                 {
+<<<<<<< HEAD
                                     }
+=======
+                    if (o instanceof Ball)
+                    {
+                        Ball b = (Ball) o;
+                        javafx.scene.shape.Circle c = new javafx.scene.shape.Circle(0,0,b.getSize());
+                        root.getChildren().add(c);
+                        balls.put(b, c);
+                    }
+
+                    /*
+                    if (o instanceof Rectangle)
+                    {
+                        Rectangle r = (Rectangle) o;
+                        javafx.scene.shape.Rectangle rectangle = new javafx.scene.shape.Rectangle(0, 0, r.getWidth(), r.getHeight());
+                        root.getChildren().add(rectangle);
+                        rectangles.put(r, rectangle);
+                    }
+                    */
+                }
+>>>>>>> parent of bcba9be (Neteja de codi i canvi en dependència errònea en el KeEvent)
 
                 addList.clear();
             }
